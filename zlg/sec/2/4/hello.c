fc2c20ebd5629b2f283d26ecc27ebfdd421f6670
sqbbingo <2577200835@qq.com>
2018-11-22 12:49:45 +0800

    Update hello.c

--- zlg/sec/2/4/hello.c
+++ zlg/sec/2/4/hello.c
@@ -1,7 +1,7 @@
 #include <linux/module.h>	//内核模块必需包含的内核头文件，动态的将模块加载到内核中去，包含MODULE_LICESENCE()等宏定义
-#include <linux/init.h>
+#include <linux/init.h>		//内核模块必需包含的内核头文件
 
-static int_init hello_init(void)
+static int_init hello_init(void)	//声明为 static， 因为初始化函数对于其它文件没有任何意义，__init 表示初始化函数仅仅在初始化期间使用
 {
 	printk("Hello,I'm ready!\n");
 	return 0;
@@ -15,4 +15,4 @@ static void_exit hello_exit(void)
 module_init(hello_init);
 module_exit(hello_exit);
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL");		//模块必须指定一个许可协议，否则会出错
